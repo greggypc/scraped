04e15b23d156aa62e8ea010ddde744a57633a962
Greg <gregpchurch@gmail.com>
2018-03-16 19:45:32 -0500

    heroku

--- server.js
+++ server.js
@@ -31,11 +31,15 @@ app.use(express.static("public"));
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
-// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/mongoScraper", {
-   //useMongoClient: true
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
 });
 
 // Start the server
