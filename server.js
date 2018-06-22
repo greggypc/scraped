4d1728d3a14257dd27cb5ff3fbc99f96d0422fea
Greg <gregpchurch@gmail.com>
2018-06-21 21:54:55 -0500

    refactor server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var logger = require("morgan");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const logger = require("morgan");
 // Require all models
-var db = require("./models");
+const db = require("./models");
 
-var PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3003;
 
 // Initialize Express
-var app = express();
+const app = express();
 
 // Configure middleware
 
@@ -16,24 +16,24 @@ var app = express();
 app.use(logger('dev'));
 
 // Handlebars
-var exphbs = require("express-handlebars");
+const exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
 // Routes
-require("./routes/api-routes.js")(app);
-require("./routes/html-routes.js")(app);
-
+const routes = require("./routes");
+// All requests go through routes
+app.use(routes);
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoScraper";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
@@ -43,6 +43,6 @@ mongoose.connect(MONGODB_URI, {
 });
 
 // Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
 });
